55faf9eeb9d102089eb7a9390db7d8693eae4f41
Marsha+ Foundation <info@marshafoundation.org>
2024-06-30 15:11:37 +0200

    Create config.js

---
+++ r config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: process.env.TELEGRAM_API_KEY,
+  openaiApiKey: process.env.OPENAI_API_KEY,
+  assistantName: 'SilvIA+',
+  assistantDescription: 'el primer asistente LGTBI+ en el mundo =) Desarrollado por Marsha+ Foundation. www.marshafoundation.org, info@marshafoundation.org.',
+  adminChatId: process.env.ADMIN_CHAT_ID,
+  databaseUrl: process.env.DATABASE_URL
+};
